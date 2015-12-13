baa8b25173ae30373526918988c880feff2edea2
Rennan Maciel <renmcoliver@gmail.com>
2015-12-13 04:29:33 -0200

    Creating a group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,7 +5,27 @@ class GroupsController < ApplicationController
   end
 
   def create
+    @group = Group.new(group_params)
+    group_maker = GroupMaker.new(current_user, @group)
+    if group_maker.save
+      redirect_to @group, notice: 'Group successfully created!'
+    else
+      render :new, alert: 'Something went wrong'
+    end
+  end
 
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def search
+
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :description)
   end
 
 end
