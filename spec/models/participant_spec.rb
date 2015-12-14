b8b7cffb93ce2035bc2a2e04af298718a2bc7bf8
Rennan Maciel <renmcoliver@gmail.com>
2015-12-13 22:38:35 -0200

    Adding participant delegation spec

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -7,11 +7,14 @@ RSpec.describe Participant, type: :model do
   describe 'relations'  do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to belong_to(:group) }
+    it { is_expected.to have_one(:secret_santa) }
   end
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:user) }
     it { is_expected.to validate_presence_of(:group) }
   end
+
+  it { is_expected.to delegate_method(:username).to(:user).as(:username) }
 
 end
